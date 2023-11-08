2ec33da716d73528ad3f9412cc04bb25c85ccee2
shaheemux <120124594+shaheemux@users.noreply.github.com>
2023-11-08 11:21:17 +0200

    added files

--- index.js
+++ index.js
@@ -31,14 +31,20 @@ console.log(`the area of the rectangle is $(area)`);
 let num1 = 10;
 let num2 = 4;
 
-console.log(`$(num1) / $(num2) = $(math.floor(num1 / num2) remainder $(remainder)`);
+let modulus = num1 % num2;
+console.log(`modulus`);
 ​
 // 4.Using the variables you created in the previous step, output the results
 // of the division in the following format: “num1 / num2 = x remainder y.” HINT: Backticks
+
+console.log(`$(num1) / $(num2) = $(math.floor(num1 / num2) remainder $(remainder)`);
+​
 ​
 ​
 // B. Task 2:Data Types
 // 1. Define the following data types with your own contents[ 'integer', 'decimal', 'string', 'array'(minimum: 3 items), 'Object'(minimum: 4 values)]
+
+
 // 2. Once you’ve created the variables, display them to the console
 // 3. Use the ‘typeof’ function to check each variable
 // 4. Create an array with numbers and strings. Comment what data type you expect this tobe. Then use the ‘typeof’ function on this array
